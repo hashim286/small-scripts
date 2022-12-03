c285609085d7182348aa24cbb76f535907e966a9
hashim286 <112742556+hashim286@users.noreply.github.com>
2022-12-03 18:49:29 -0500

    Create time_extract.py
    pulls out just the times listed in video title and adds them up for an estimate for how long the videos will take to finish

---
+++ time-estimate/time_extract.py
@@ -0,0 +1,28 @@
+import re as re
+from text import text
+
+
+class Extraction:
+
+    def __init__(self):
+        self.input = text
+        self.times = []
+        self.total = 0
+
+    def extract_times(self):
+        # \\d pulls out digits in the text self.input, the + is meant to specify more than one digit if any exist, 
+        # repeat this for the other side of the colon in timestamps
+        self.times = re.findall('\\d+:\\d+', self.input)
+
+    def separate_mins_from_seconds(self):
+        for item in self.times:
+            times_separated = item.split(':')
+            seconds = int(times_separated[1])/60
+            minutes = float(times_separated[0])
+            self.total += round(seconds + minutes, 2)
+
+    def convert_to_hours_and_minutes(self):
+        convert = int(self.total)
+        hours = int(convert/60)
+        minutes = convert % 60
+        print(f"This section should take {hours} hour(s) and {minutes} minute(s) to finish")
