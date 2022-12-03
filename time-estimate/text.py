1996cd9bf9031339dc18a4ab96d15ce09ab08340
hashim286 <112742556+hashim286@users.noreply.github.com>
2022-12-03 18:48:17 -0500

    Create text.py

---
+++ time-estimate/text.py
@@ -0,0 +1,19 @@
+#paste video titles here, example below
+text = '''
+
+
+Rogue Access Points and Evil Twins (3:43)
+Bluejacking and Bluesnarfing (2:09)
+Wireless Disassociation Attacks (5:50)
+Wireless Jamming (3:15)
+RFID and NFC Attacks (4:00)
+Randomizing Cryptography (4:18)
+On-Path Attacks (6:04)
+MAC Flooding and Cloning (7:53)
+DNS Attacks (8:19)
+Denial of Service (8:35)
+Malicious Scripts (4:47)
+
+
+'''
+
